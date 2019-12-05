33ac931fdab0e8e429994b76a0590050f80d4e53
Ocraz <registered@quicknet.nl>
2019-12-05 09:40:56 +0100

    Add files via upload

---
+++ GoogleZorks-Forms.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start "" "https://www.google.com/search?q=%1+%2+site:https://ssl1.peoplexs.com&newwindow=1"
+
+start "" "https://www.google.com/search?q=%1+%2++site:formdesk.com+%%7C+site:formlets.com+%%7C+site:123formbuilder.com+%%7C+site:zoho.com+%%7C+site:formsite.com+%%7C+site:123formbuilder.com+%%7C+site:form.jotform.com+%%7C+site:cloudformz.com+%%7C+site:formstack.com+%%7C+site:wufoo.com+%%7C+site:cognitoforms.com+%%7C+site:forms.office.com+%%7C+site:google.com/forms+%%7C+site:forms.office.com+-site:www.formsite.com&newwindow=1"
+
+start "" "https://www.google.com/search?q=%1+%2+site:prezi.com+%%7C+site:amazonaws.com+%%7C+site:webflow.com+%%7C+site:hubspot.net+%%7C+site:cloudfront.net+%%7C+site:visme.co+%%7C+site:slidebean.com+%%7C+site:slideshare.net+%%7C+site:emaze.com+%%7C+site:slides.com+%%7C+site:haikudeck.com+%%7C+site:sway.office.com+%%7C+site:ecitydoc.com+%%7C+site:tenderned.nl+%%7C+site:multiscreensite.com+%%7C+site:zonebourse.com+%%7C+site:slidex.tips+%%7C+site:ziladoc.com&newwindow=1"
