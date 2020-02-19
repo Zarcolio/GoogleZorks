8750c421574bf6efdc33d672b77d8db3929a83ad
Zarcolio <registered@quicknet.nl>
2020-02-19 21:23:01 +0100

    Add files via upload

--- GoogleZorks-WebTech-Filetypes.bat
+++ GoogleZorks-WebTech-Filetypes.bat
@@ -2,9 +2,8 @@
 "c:\Program Files (x86)\Mozilla Firefox\firefox.exe" -new-window
 timeout /t 3
 
-start "" "https://www.google.nl/search?num=100&newwindow=1&q=site:%1+filetype:php+%%7C+filetype:php2+%%7C+filetype:php3+%%7C+filetype:php4+%%7C+filetype:php5+%%7C+filetype:phps+%%7C+filetype:cfm+%%7C+%%7C+filetype:inc+%%7C+filetype:db+%%7C+filetype:src+%%7C+filetype:pl+%%7C+filetype:cgi+%%7C+filetype:dll+%%7C+filetype:shtml+%%7C+filetype:phtml+%%7C+filetype:xhtml+%%7C+%%7C+filetype:shtm+%%7C+filetype:phtm+%%7C+filetype:xhtm+%%7C+filetype:jhtm+%%7C+filetype:xml+%%7C+filetype:py+%%7C+filetype:swf+%%7C+filetype:action+%%7C+filetype:nsf+%%7C+filetype:fpl+%%7C+filetype:fcgi+-filetype:pdf"
+start "" "https://www.google.nl/search?num=100&newwindow=1&q=site:%1+filetype:php+%%7C+filetype:php2+%%7C+filetype:php3+%%7C+filetype:php4+%%7C+filetype:php5+%%7C+filetype:phps+%%7C+filetype:cfm+%%7C+filetype:inc+%%7C+filetype:db+%%7C+filetype:src+%%7C+filetype:pl+%%7C+filetype:cgi+%%7C+filetype:dll+%%7C+filetype:shtml+%%7C+filetype:phtml+%%7C+filetype:xhtml+%%7C+%%7C+filetype:shtm+%%7C+filetype:phtm+%%7C+filetype:xhtm+%%7C+filetype:jhtm+%%7C+filetype:xml+%%7C+filetype:py+%%7C+filetype:rss+%%7C+filetype:atom+%%7C+filetype:action+%%7C+filetype:nsf+%%7C+filetype:fpl+%%7C+filetype:fcgi+-filetype:pdf&filter=0"
 
-start "" "https://www.google.nl/search?num=100&newwindow=1&q=site:%1+filetype:jsf+%%7C+filetype:java+%%7C+filetype:do+%%7C+filetype:jhtml+%%7C+filetype:jsp+%%7C+filetype:jspa+%%7C+filetype:jspx+%%7C+filetype:jst+%%7C+filetype:jws"
-start "" "https://www.google.nl/search?num=100&newwindow=1&q=site:%1+filetype:ASP+%%7C+filetype:ASPX+%%7C+filetype:ASHX+%%7C+filetype:ASAX+%%7C+filetype:ASCMX+%%7C+filetype:ASA+%%7C+filetype:ASD+%%7C+filetype:AXD+%%7C+filetype:BROWSER+%%7C+filetype:CSHTML+%%7C+filetype:MASTER+%%7C+filetype:MSPX+%%7C+filetype:SITEMAP+%%7C+filetype:VBMTHL"
+start "" "https://www.google.nl/search?num=100&newwindow=1&q=site:%1+filetype:jsf+%%7C+filetype:java+%%7C+filetype:do+%%7C+filetype:jhtml+%%7C+filetype:jsp+%%7C+filetype:jspa+%%7C+filetype:jspx+%%7C+filetype:jst+%%7C+filetype:jws&filter=0"
 
-
+start "" "https://www.google.nl/search?num=100&newwindow=1&q=site:%1+filetype:ASP+%%7C+filetype:ASPX+%%7C+filetype:ASHX+%%7C+filetype:ASCX+%%7C+filetype:ASMX+%%7C+filetype:ASX+%%7C+filetype:ASAX+%%7C+filetype:ASCMX+%%7C+filetype:ASA+%%7C+filetype:ASD+%%7C+filetype:AXD+%%7C+filetype:BROWSER+%%7C+filetype:CSHTML+%%7C+filetype:MASTER+%%7C+filetype:MSPX+%%7C+filetype:SITEMAP+%%7C+filetype:VBMTHL&filter=0"
