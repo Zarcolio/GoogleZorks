feb54e275f6e627d5ca3931186a4916f63f5e113
Zarcolio <registered@quicknet.nl>
2020-03-29 00:53:14 +0100

    Add files via upload

--- GoogleZorks-Forms.bat
+++ GoogleZorks-Forms.bat
@@ -7,3 +7,4 @@ start "" "https://www.google.com/search?q=%1+%2++site:formdesk.com+%%7C+site:for
 start "" "https://www.google.com/search?q=%1+%2+site:prezi.com+%%7C+site:amazonaws.com+%%7C+site:core.windows.net+%%7C+site:webflow.com+%%7C+site:hubspot.net+%%7C+site:cloudfront.net+%%7C+site:visme.co+%%7C+site:slidebean.com+%%7C+site:slideshare.net+%%7C+site:emaze.com+%%7C+site:slides.com+%%7C+site:haikudeck.com+%%7C+site:sway.office.com+%%7C+site:ecitydoc.com+%%7C+site:tenderned.nl+%%7C+site:multiscreensite.com+%%7C+site:zonebourse.com+%%7C+site:slidex.tips+%%7C+site:ziladoc.com+%%7C+site:trello.com&newwindow=1"
 
 
+
